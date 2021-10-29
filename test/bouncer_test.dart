175e84ac1b4f5e91f2020785660cd37bb5ab66c6
Vladimir Goldobin <bobagold@gmail.com>
2021-10-29 08:56:50 +0200

    improve naming

--- test/bouncer_test.dart
+++ test/bouncer_test.dart
@@ -1,14 +1,13 @@
 import 'dart:async';
 
-import 'package:test/test.dart';
-
 import 'package:bouncer/bouncer.dart';
+import 'package:test/test.dart';
 
 void main() {
   Future<int> fastRequest() => Future.value(3);
   Future<int> slowRequest() => Future.delayed(Duration(seconds: 3), () => 3);
 
-  test('NoBouncer lets everyone', () {
+  test('NoBouncer lets everyone in', () {
     final bouncer = NoBouncer();
     var completer = Completer();
     var subscription = bouncer.debounce(
@@ -18,67 +17,70 @@ void main() {
     expect(subscription, null);
     expect(completer.future, completion(equals(3)));
   });
-  test('TimerBouncer lets everyone', () {
-    final bouncer = TimerBouncer(Duration.zero);
-    var completer = Completer();
-    var subscription = bouncer.debounce(
-      request: fastRequest,
-      responseHandler: completer.complete,
-    );
-    expect(subscription, isA<Subscription>());
-    expect(completer.future, completes);
-  });
-  test('double cancel makes no harm', () {
-    final bouncer = TimerBouncer(Duration.zero);
-    var completer = Completer();
-    var subscription = bouncer.debounce(
-      request: fastRequest,
-      responseHandler: completer.complete,
-    );
-    subscription.cancel();
-    subscription.cancel();
-  });
-  test('TimerBouncer allows to cancel responseHandler', () {
-    final bouncer = TimerBouncer(Duration.zero);
-    var completer = Completer();
-    var subscription = bouncer.debounce(
-      request: slowRequest,
-      responseHandler: completer.complete,
-    );
-    subscription.cancel();
-    expect(completer.future, doesNotComplete);
-  });
-  test('TimerBouncer actually postpones request', () {
-    final bouncer = TimerBouncer(Duration(seconds: 3));
-    var completer1 = Completer();
-    var completer2 = Completer();
-    Future<int> fastRequestThatWillNotRun() {
-      completer1.complete();
-      return Future.value(3);
-    }
 
-    var subscription = bouncer.debounce(
-      request: fastRequestThatWillNotRun,
-      responseHandler: completer2.complete,
-    );
-    expect(completer1.future, doesNotComplete);
-    expect(completer2.future, doesNotComplete);
-    subscription.cancel();
-  });
-  test('TimerBouncer cancels previous subscription', () {
-    final bouncer = TimerBouncer(Duration.zero);
-    var completer1 = Completer();
-    var completer2 = Completer();
-    var subscription = bouncer.debounce(
-      request: slowRequest,
-      responseHandler: completer1.complete,
-    );
-    bouncer.debounce(
-      request: slowRequest,
-      responseHandler: completer2.complete,
-      oldSubscription: subscription,
-    );
-    expect(completer1.future, doesNotComplete);
-    expect(completer2.future, completes);
+  group('TimerBouncer', () {
+    test('it lets everyone in', () {
+      final bouncer = TimerBouncer(Duration.zero);
+      var completer = Completer();
+      var subscription = bouncer.debounce(
+        request: fastRequest,
+        responseHandler: completer.complete,
+      );
+      expect(subscription, isA<Subscription>());
+      expect(completer.future, completes);
+    });
+    test('double cancel makes no harm', () {
+      final bouncer = TimerBouncer(Duration.zero);
+      var completer = Completer();
+      var subscription = bouncer.debounce(
+        request: fastRequest,
+        responseHandler: completer.complete,
+      );
+      subscription.cancel();
+      subscription.cancel();
+    });
+    test('it allows to cancel responseHandler', () {
+      final bouncer = TimerBouncer(Duration.zero);
+      var completer = Completer();
+      var subscription = bouncer.debounce(
+        request: slowRequest,
+        responseHandler: completer.complete,
+      );
+      subscription.cancel();
+      expect(completer.future, doesNotComplete);
+    });
+    test('it actually postpones request', () {
+      final bouncer = TimerBouncer(Duration(seconds: 3));
+      var completer1 = Completer();
+      var completer2 = Completer();
+      Future<int> fastRequestThatWillNotRun() {
+        completer1.complete();
+        return Future.value(3);
+      }
+
+      var subscription = bouncer.debounce(
+        request: fastRequestThatWillNotRun,
+        responseHandler: completer2.complete,
+      );
+      expect(completer1.future, doesNotComplete);
+      expect(completer2.future, doesNotComplete);
+      subscription.cancel();
+    });
+    test('it cancels previous subscription', () {
+      final bouncer = TimerBouncer(Duration.zero);
+      var completer1 = Completer();
+      var completer2 = Completer();
+      var subscription = bouncer.debounce(
+        request: slowRequest,
+        responseHandler: completer1.complete,
+      );
+      bouncer.debounce(
+        request: slowRequest,
+        responseHandler: completer2.complete,
+        oldSubscription: subscription,
+      );
+      expect(completer1.future, doesNotComplete);
+      expect(completer2.future, completes);
+    });
   });
 }
